ce1d8a71c5ae8f52c1f8dee04cacd2f63eb8ae2b
pangratz <cmueller.418@gmail.com>
2012-02-01 13:05:30 +0100

    add basic ECGMonitor implementation

--- src/main/java/at/jku/pervasive/ecg/ECGMonitor.java
+++ src/main/java/at/jku/pervasive/ecg/ECGMonitor.java
@@ -1,17 +1,77 @@
 package at.jku.pervasive.ecg;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 import javax.swing.JFrame;
 
-public class ECGMonitor extends JFrame {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
 
-  public static void main(String[] args) {
-    new ECGMonitor().setVisible(true);
+public class ECGMonitor extends JFrame {
+  private class ListenForUpdates extends Thread implements IHeartManListener {
+
+    private final BlockingQueue<Double> buffer;
+    private final TimeSeries series;
+
+    public ListenForUpdates(TimeSeries series) {
+      super();
+      this.series = series;
+      this.buffer = new LinkedBlockingQueue<Double>();
+    }
+
+    @Override
+    public void dataReceived(String address, double value) {
+      this.buffer.add(value);
+    }
+
+    @Override
+    public void run() {
+      while (!isInterrupted()) {
+        try {
+          Double value = this.buffer.take();
+          this.series.addOrUpdate(new Millisecond(), value);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
   }
+
+  private final ListenForUpdates listenForUpdates;
 
   public ECGMonitor() {
     super();
 
     setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    TimeSeries timeSeries = new TimeSeries("ecg");
+    long _30seconds = TimeUnit.SECONDS.toMillis(10);
+    System.out.println(_30seconds);
+    timeSeries.setMaximumItemAge(_30seconds);
+
+    XYDataset dataset = new TimeSeriesCollection(timeSeries);
+    JFreeChart chart = ChartFactory.createTimeSeriesChart("ecg", "time", "V",
+        dataset, false, false, false);
+    ChartPanel chartPanel = new ChartPanel(chart);
+    add(chartPanel);
+
+    listenForUpdates = new ListenForUpdates(timeSeries);
+    listenForUpdates.start();
+
+    pack();
+
+    // center on screen
+    setLocationRelativeTo(null);
   }
 
+  public IHeartManListener getHeartManListener() {
+    return listenForUpdates;
+  }
 }
---
+++ src/test/java/at/jku/pervasive/ecg/ECGMonitorTest.java
@@ -0,0 +1,45 @@
+package at.jku.pervasive.ecg;
+
+import junit.framework.TestCase;
+
+public class ECGMonitorTest extends TestCase {
+
+  private HeartManDiscovery heartManDiscovery;
+  private HeartManSimulator heartManSimulator;
+
+  public void testECGMonitor() throws Exception {
+    String address = heartManSimulator.createDevice();
+    heartManDiscovery.discoverHeartManDevices();
+
+    ECGMonitor ecgMonitor = new ECGMonitor();
+    ecgMonitor.setVisible(true);
+
+    heartManDiscovery.startListening(address, ecgMonitor.getHeartManListener());
+
+    while (true) {
+      heartManSimulator.sendValue(address, Math.random() - 0.5D);
+      Thread.sleep((long) (Math.random() * 100 + 100));
+    }
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    heartManSimulator = new HeartManSimulator();
+
+    heartManDiscovery = new HeartManDiscovery();
+    assertTrue(heartManDiscovery.isBluetoothEnabled());
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+
+    heartManDiscovery = null;
+
+    heartManSimulator.stopServer();
+    heartManSimulator = null;
+  }
+
+}

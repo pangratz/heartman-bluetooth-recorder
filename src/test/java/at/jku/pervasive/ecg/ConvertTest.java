23aa5e9ad4cacc37fa0c62ebf822319aab74941f
pangratz <cmueller.418@gmail.com>
2012-02-02 20:50:53 +0100

    remove commons-codec dependency

--- src/test/java/at/jku/pervasive/ecg/ConvertTest.java
+++ src/test/java/at/jku/pervasive/ecg/ConvertTest.java
@@ -4,18 +4,16 @@ import java.io.ByteArrayInputStream;
 
 import junit.framework.TestCase;
 
-import org.apache.commons.codec.binary.Hex;
-
 public class ConvertTest extends TestCase {
 
   public void testConvertHexToFloat() throws Exception {
-    byte[] data = new byte[] { (byte) 254, (byte) 112 };
-    String encodeHexString = Hex.encodeHexString(data);
+    String encodeHexString = "FE70";
     System.out.println(encodeHexString);
     short value = Integer.valueOf(encodeHexString, 16).shortValue();
     assertEquals(-400, value);
 
     // http://docs.oracle.com/javase/1.5.0/docs/api/java/io/DataInput.html#readShort()
+    byte[] data = new byte[] { (byte) 254, (byte) 112 };
     byte a = data[0], b = data[1];
     short secondValue = (short) ((a << 8) | (b & 0xff));
     System.out.println(secondValue);
